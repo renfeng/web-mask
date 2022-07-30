ce1e80f83204d9f8a8abbd58d35d80ab9fb8c95a
Frank <Frank.RenFeng@swisscom.com>
2022-07-30 13:01:25 +0200

    Copied from https://git.swisscom.com/projects/OCE/repos/care-frontend/browse/tools/chrome-extension?at=5e9657cb34c1982d62a668832268f0ba52d764f9

---
+++ background.js
@@ -0,0 +1,20 @@
+const page = chrome.extension.getBackgroundPage();
+chrome.storage.local.get('careEnabled', function (val) {
+  const enabled = val && val.careEnabled;
+  page.angularInjectionEnabled = enabled;
+  chrome.browserAction.setIcon({
+    path: enabled ? 'icon128.png' : 'off-icon128.png',
+  });
+});
+
+chrome.webRequest.onBeforeRequest.addListener(
+  function (info) {
+    return { cancel: page.angularInjectionEnabled };
+  },
+  {
+    urls: ['*://*.swisscom.ch/myswisscom/*.js', '*://*.swisscom.com/myswisscom/*.js', '*://*.swissptt.ch/myswisscom/*.js'],
+  },
+  ['blocking', 'requestBody']
+);
+
+page.console.log('Swisscom Angular Frontend Switcher initialized');
