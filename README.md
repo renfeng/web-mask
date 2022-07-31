e889a9f001a21861a2d7039a7d132417538304ec
Frank <Frank.RenFeng@swisscom.com>
2022-07-31 20:41:44 +0200

    Updated to manifest v3

--- README.md
+++ README.md
@@ -10,25 +10,18 @@ This gives us the following possibilities:
 
 ## Build
 
-Run `bin/build.sh`
+Run `bin/build.sh ${path_to_dist} ${domain}`
 
-## Install
+## Install and run
 
 1. Open `chrome://extensions` in your Chrome browser
 2. Enable `Developer mode`
 3. Click `Load unpacked` and point it to `dist/unpacked`
-4. A grayscale Swisscom icon appears in the upper right corner of Chrome, click it and press "Enable" to activate the plugin
-5. The Swisscom logo is now colorful, meaning the plugin is active
+4. A grayscale Swisscom icon appears in the upper right corner of Chrome, which indicates the plugin is disable for current tab.
+5. Open a tab with the given domain, the Swisscom logo is now colorful, meaning the plugin is active.
 
-## How to run it
+## Uninstall or disable
 
-1.  `npm run start:extension`
-2.  Open the frontend where you want the scripts injected on any environment, e.g. Vega
-3.  Open the console - you should now see a message `Injected Local Angular Scripts!`
-4.  Perform integration tests
+When done you can uninstall the plugin by clicking the colorful Swisscom logo in the upper right corner of chrome and press "Remove from Chrome..."
 
-## Disabling plugin
-
-1. When done you can disable the plugin by clicking the colorful Swisscom logo in the upper right corner of chrome and press "disable".
-2. The plugin is now inactive and the normal environment scripts will be loaded when navigating
-3. If you want you can also disable or remove the plugin entirely under `chrome://extensions`
+You can also disable the plugin under `chrome://extensions`
--- bin/build.sh
+++ bin/build.sh
@@ -2,16 +2,47 @@
 
 set -e
 
-pushd "$(dirname "$0")/.." > /dev/null
+pushd "$(dirname "$0")/.." >/dev/null
+
+source=${1}
+domain=${2}
+
+domains=("${domain}")
+domain_array=$(printf '%s\n' "${domains[@]}" | jq -R . | jq -s .)
+
+urls=("*://${domain}/*")
+url_array=$(printf '%s\n' "${urls[@]}" | jq -R . | jq -s .)
 
 rm -rf dist
 mkdir -p dist/unpacked
-cp src/manifest.json dist/unpacked
+
+cp -r "${source}/." dist/unpacked
+
+#js_files=$(perl -nle 'print for /(?<=<script src=")[^"]+(?=" type="module">)/g' <dist/unpacked/index.html)
+js_files=$(find dist/unpacked/ -type f -name "*.js" | sed 's|dist/unpacked/||')
+js_array=$(printf '%s\n' "${js_files[@]}" | jq -R . | jq -s .)
+
+#css_files=$(perl -nle 'print for /(?<=<link href=")[^"]+(?=[?]\w+" rel="stylesheet">)/g' < dist/unpacked/index.html)
+#css_files+=(styles.css)
+css_files=$(find dist/unpacked/ -type f -name "*.css" | sed 's|dist/unpacked/||')
+css_array=$(printf '%s\n' "${css_files[@]}" | jq -R . | jq -s .)
+
+manifest=$(cat src/manifest.json)
+manifest=$(echo "${manifest}" | jq '.content_scripts[0].matches'="${url_array}")
+manifest=$(echo "${manifest}" | jq '.content_scripts[0].js'="${js_array}")
+manifest=$(echo "${manifest}" | jq '.content_scripts[0].css'="${css_array}")
+manifest=$(echo "${manifest}" | jq '.host_permissions'="${url_array}")
+echo "${manifest}" >dist/unpacked/manifest.json
+
+rules=$(cat src/rules.json)
+rules=$(echo "${rules}" | jq '.[0].condition.domains'="${domain_array}")
+echo "${rules}" >dist/unpacked/rules.json
+
 cp -r src/vanilla/. dist/unpacked
 
-bin/version.sh > dist/unpacked/version.txt
+bin/version.sh >dist/unpacked/version.txt
 
 cd dist/unpacked
 zip -r ../chrome-extension.zip .
 
-popd > /dev/null
+popd >/dev/null
