d4fa1157b7b421912ea4227e97f9cf25a9c6c7a3
Frank <Frank.RenFeng@swisscom.com>
2022-08-08 19:38:13 +0200

    Cleanup version script

--- bin/version.sh
+++ bin/version.sh
@@ -3,25 +3,17 @@
 # exit on any error
 set -e
 
-pushd "$(dirname "$0")/.." > /dev/null
+pushd "$(dirname "$0")/.." >/dev/null
 
-if ! version=$(git describe --exact-match --tags 2> /dev/null); then
-  # A git hash of the length of 10 is visible on the github license page. See CREDIT.md
-  GIT_HASH_LENGTH=10
-  if ! version=$(git rev-parse --short=${GIT_HASH_LENGTH} HEAD 2> /dev/null); then
-    echo
-  fi
+if ! version=$(git describe --exact-match --tags 2>/dev/null); then
+  # A git hash of the length of 11 is visible on bitbucket commits page.
+  GIT_HASH_LENGTH=11
+  version=$(git rev-parse --short=${GIT_HASH_LENGTH} HEAD 2>/dev/null) || true
 fi
 
-if [ "${version}" ]; then
-  echo "${version}"
-  echo "https://git.swisscom.com/users/taarefr1/repos/chrome-extension/commits/${version}"
-  echo "$(whoami)@$(hostname)"
-  git status
-else
-  echo ""
-  echo "https://git.swisscom.com/users/taarefr1/repos/chrome-extension/browse"
-  echo "$(whoami)@$(hostname)"
-fi
+echo "${version}"
+echo "https://git.swisscom.com/users/taarefr1/repos/chrome-extension/commits/${version}"
+echo "$(whoami)@$(hostname)"
+git status
 
-popd > /dev/null
+popd >/dev/null
