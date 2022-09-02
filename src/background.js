9adec8b045a8e9c6b5c345749a0c87bdd5443cc0
Frank <Frank.RenFeng@swisscom.com>
2022-09-03 01:10:54 +0200

    Error handling when dev server is not running

--- src/background.js
+++ src/background.js
@@ -5,10 +5,16 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   } = sender;
   try {
     if (message.action === 'fetch') {
-      fetch(new URL(message.src, 'http://localhost:PORT'))
+      const url = new URL(message.src, 'http://localhost:PORT');
+      fetch(url)
         .then((response) => response.text())
         .then((content) => {
           chrome.tabs.sendMessage(tabId, { action: message.next, content }, (response) => {
+            console.debug('response received', response);
+          });
+        })
+        .catch((error) => {
+          chrome.tabs.sendMessage(tabId, { action: 'error', content: `${error.message} ${url}` }, (response) => {
             console.debug('response received', response);
           });
         });
--- src/vanilla/content.js
+++ src/vanilla/content.js
@@ -2,54 +2,14 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   console.log(`message received: ${JSON.stringify(message, null, 2)}, ${JSON.stringify(sender, null, 2)}`);
   try {
     if (message.action === 'html') {
-      const { content: html } = message;
-
-      document.head.querySelectorAll('link[href]').forEach((node) => node.remove());
-      const head = html.match(/<head>.*<[/]head>/s)[0];
-      head.match(/<link\b.*?>/g).forEach((link) => {
-        const href = link.match(/(?<=href=")(?<href>[^"]+)(?=")/)?.groups['href'];
-        if (!href) {
-          return;
-        }
-
-        const element = document.createElement('link');
-        element.href = href;
-
-        const type = link.match(/(?<=type=")(?<type>[^"]+)(?=")/)?.groups['type'];
-        if (type) {
-          element.type = type;
-        }
-
-        const rel = link.match(/(?<=rel=")(?<rel>[^"]+)(?=")/)?.groups['rel'];
-        if (rel) {
-          element.rel = rel;
-        }
-
-        document.head.appendChild(element);
-      });
-
-      document.body.querySelectorAll('script[src]').forEach((node) => node.remove());
-      const body = html.match(/<body>.*<[/]body>/s)[0];
-      body.match(/<script\b.*?><\/script>/g).forEach((script) => {
-        const src = script.match(/(?<=src=")(?<src>[^"]+)(?=")/)?.groups['src'];
-        if (!src) {
-          return;
-        }
-
-        const type = script.match(/(?<=type=")(?<type>[^"]+)(?=")/)?.groups['type'];
-        if (type === 'module') {
-          const element = document.createElement('script');
-          element.src = src;
-          element.type = 'module';
-          document.body.appendChild(element);
-        } else {
-          chrome.runtime.sendMessage({ action: 'fetch', src, next: 'javascript' }, callback);
-        }
-      });
+      filterHTML(message.content);
       sendResponse('success');
     } else if (message.action === 'javascript') {
-      const { content: javascript } = message;
-      window.postMessage({ action: 'eval', javascript }, location.origin);
+      injectJavascript(message.content);
+      sendResponse('success');
+    } else if (message.action === 'error') {
+      alert(message.content);
+      sendResponse('success');
     } else {
       sendResponse('not implemented');
     }
@@ -71,4 +31,58 @@ function callback(response) {
   } else if (chrome.runtime.lastError) {
     console.error('error occurred', chrome.runtime.lastError);
   }
+}
+
+function filterHTML(html) {
+  filterHead(html.match(/<head>.*<[/]head>/s)[0]);
+  filterBody(html.match(/<body>.*<[/]body>/s)[0]);
+}
+
+function filterHead(head) {
+  document.head.querySelectorAll('link[href]').forEach((node) => node.remove());
+  head.match(/<link\b.*?>/g).forEach((link) => {
+    const href = link.match(/(?<=href=")(?<href>[^"]+)(?=")/)?.groups['href'];
+    if (!href) {
+      return;
+    }
+
+    const element = document.createElement('link');
+    element.href = href;
+
+    const type = link.match(/(?<=type=")(?<type>[^"]+)(?=")/)?.groups['type'];
+    if (type) {
+      element.type = type;
+    }
+
+    const rel = link.match(/(?<=rel=")(?<rel>[^"]+)(?=")/)?.groups['rel'];
+    if (rel) {
+      element.rel = rel;
+    }
+
+    document.head.appendChild(element);
+  });
+}
+
+function filterBody(body) {
+  document.body.querySelectorAll('script[src]').forEach((node) => node.remove());
+  body.match(/<script\b.*?><\/script>/g).forEach((script) => {
+    const src = script.match(/(?<=src=")(?<src>[^"]+)(?=")/)?.groups['src'];
+    if (!src) {
+      return;
+    }
+
+    const type = script.match(/(?<=type=")(?<type>[^"]+)(?=")/)?.groups['type'];
+    if (type === 'module') {
+      const element = document.createElement('script');
+      element.src = src;
+      element.type = 'module';
+      document.body.appendChild(element);
+    } else {
+      chrome.runtime.sendMessage({ action: 'fetch', src, next: 'javascript' }, callback);
+    }
+  });
+}
+
+function injectJavascript(javascript) {
+  window.postMessage({ action: 'eval', javascript }, location.origin);
 }
