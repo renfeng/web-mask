bb8f3725a092ad71d3a3d6ad7124d54ff28ae352
Frank <Frank.RenFeng@swisscom.com>
2022-09-05 16:05:44 +0200

    Updated version script

--- bin/version.sh
+++ bin/version.sh
@@ -6,13 +6,12 @@ set -e
 pushd "$(dirname "$0")/.." >/dev/null
 
 if ! version=$(git describe --exact-match --tags 2>/dev/null); then
-  # A git hash of the length of 11 is visible on bitbucket commits page.
-  GIT_HASH_LENGTH=11
+  # A git hash of the length of 7 is visible on github commits page.
+  GIT_HASH_LENGTH=7
   version=$(git rev-parse --short=${GIT_HASH_LENGTH} HEAD 2>/dev/null) || true
 fi
 
 echo "${version}"
-echo "https://git.swisscom.com/users/taarefr1/repos/chrome-extension/commits/${version}"
 echo "$(whoami)@$(hostname)"
 git status
 
