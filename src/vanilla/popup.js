bc830ec6a0822445920de9c16d8847afc8138ff2
Frank <Frank.RenFeng@swisscom.com>
2022-09-12 09:28:59 +0200

    Made popup more informative and interactive

--- src/vanilla/popup.js
+++ src/vanilla/popup.js
@@ -21,15 +21,19 @@ chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
   chrome.runtime.sendMessage({ action: 'ping', tab }, (response) => {
     if (response) {
       console.debug('response received', JSON.stringify(response, null, 2));
-      const { enabled, path } = response;
+      const { enabled, path, port } = response;
       toggleButton.innerText = enabled ? 'Disable' : 'Enable';
 
       const defaultPath = document.getElementById('default-path');
+      const input = defaultPath.querySelector('input');
       if ('/' === path || !path) {
-        defaultPath.querySelector('input').setAttribute('checked', 'checked');
+        input.setAttribute('checked', 'checked');
       } else {
-        defaultPath.querySelector('input').removeAttribute('checked');
+        input.removeAttribute('checked');
       }
+      input.addEventListener('click', (event) => {
+        document.querySelector('.path-text').innerText = event.target.value;
+      });
 
       if (tab.url) {
         new URL(tab.url).pathname
@@ -44,18 +48,32 @@ chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
             const clone = defaultPath.cloneNode(deep);
             clone.removeAttribute('id');
 
+            const input = clone.querySelector('input');
             const next = `${current}${component}/`;
             if (next === path) {
-              clone.querySelector('input').setAttribute('checked', 'checked');
+              input.setAttribute('checked', 'checked');
+              document.querySelector('.path-text').innerText = path;
             } else {
-              clone.querySelector('input').removeAttribute('checked');
+              input.removeAttribute('checked');
             }
-            clone.querySelector('input').value = next;
+            input.value = next;
+            input.addEventListener('click', (event) => {
+              document.querySelector('.path-text').innerText = event.target.value;
+            });
+
             clone.querySelector('span').innerText = next;
             defaultPath.parentElement.appendChild(clone);
             return next;
           }, '/');
       }
+
+      if (port) {
+        document.getElementById('port').value = port;
+        document.querySelector('.port-text').innerText = port;
+      }
+      document.getElementById('port').addEventListener('input', (event) => {
+        document.querySelector('.port-text').innerText = event.target.value;
+      });
     } else if (chrome.runtime.lastError) {
       console.error('error occurred', chrome.runtime.lastError);
     }
